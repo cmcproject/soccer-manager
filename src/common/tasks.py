edb38da68e1089fb2e0652ea3ff942dba0df34f4
Marius-Crisan <marius.crisan.tech@gmail.com>
2024-02-05 14:47:24 +0200

    Add anchor to docker-compose

--- src/common/tasks.py
+++ src/common/tasks.py
@@ -1,9 +1,14 @@
+import logging
+
 from celery import shared_task
+
+logger = logging.getLogger(__name__)
 
 
 @shared_task
 def do_something_task(arg1, arg2):
-    print("Do something...")
+    logger.info("Do something...")
+
     result = arg1 + arg2
 
     return result
